75b4e71169d7970f4dcd0ac96bb9cf830fc58b97
Brian Shimanuki <brianshimanuki@gmail.com>
2019-10-20 15:58:25 -0700

    use wordplays and acrossndown

--- python/clues.py
+++ python/clues.py
@@ -2,6 +2,7 @@ import asyncio
 from collections import defaultdict
 import enum
 import functools
+import random
 import re
 import urllib.parse
 import warnings
@@ -12,12 +13,6 @@ from typing import cast, Any, Awaitable, Dict, Optional, Union
 '''
 Scoring ported from crows project at https://github.com/kcaze/crows.
 '''
-
-
-def urlformat(text : str, dash=False) -> str:
-	if dash:
-		text = re.sub('\s', '-', text)
-	return urllib.parse.quote(text)
 
 
 async def gather_resolve_dict(d : Dict[Any, Awaitable[Any]]) -> Dict[Any, Any]:
@@ -42,44 +37,50 @@ class AsyncNoop(object):
 
 class TrackerBase(object):
 	method = None # type: Optional[str]
-	num_tries = 3
+	num_trials = 3
 	semaphore = AsyncNoop() # type: Union[AsyncNoop, asyncio.Semaphore]
-
-	@classmethod
-	def url(cls, clue : str) -> str:
+	def __init__(self, clue : str, session : aiohttp.ClientSession):
+		self.clue = clue
+		self.session = session
+		self.trial = 0
+	def url(self) -> str:
 		raise NotImplementedError()
-	@classmethod
-	def formdata(cls, clue : str) -> aiohttp.FormData:
+	def formdata(self) -> aiohttp.FormData:
 		raise NotImplementedError()
-	@classmethod
-	async def get_scores(cls, clue : str, session : aiohttp.ClientSession) -> Dict[str, float]:
+	async def get_scores(self) -> Dict[str, float]:
 		raise NotImplementedError()
-	@classmethod
-	async def fetch(cls, clue, session : aiohttp.ClientSession) -> str:
-		assert cls.method in ('get', 'post')
-		url = cls.url(clue)
-		async with cls.semaphore:
-			for _try in range(cls.num_tries):
-				if cls.method == 'get':
-					task = session.get(url)
-				else:
-					assert cls.method == 'post'
-					formdata = cls.formdata(clue)
-					task = session.post(url, data=formdata)
+	def url_clue(self, dash=False) -> str:
+		text = self.clue
+		if dash:
+			text = re.sub('\s', '-', text)
+		return urllib.parse.quote(text)
+	async def fetch(self) -> str:
+		assert self.method in ('get', 'post')
+		url = self.url()
+		for _trial in range(self.num_trials):
+			self.trial = _trial
+			if self.method == 'get':
+				task = self.session.get(url)
+			else:
+				assert self.method == 'post'
+				formdata = self.formdata()
+				task = self.session.post(url, data=formdata)
+			async with self.semaphore:
 				try:
 					async with task as response:
 						html = await response.text()
 					return html
 				except:
 					pass
-		warnings.warn('Failed connection to {} after {} tries... skipping'.format(cls.__name__, cls.num_tries))
+		warnings.warn('Failed connection to {} after {} tries... skipping'.format(self.__name__, self.num_trials))
 		return ''
+
 
 class Tracker(enum.Enum):
 	@classmethod
 	async def get_scores_by_tracker(cls, clue : str, session : aiohttp.ClientSession) -> Dict[str, Dict[str, float]]:
 		scores = {
-			tracker.name: tracker.value.get_scores(clue, session)
+			tracker.name: tracker.value(clue, session).get_scores()
 			for tracker in cls
 		}
 		tracker_scores = await gather_resolve_dict(scores)
@@ -98,17 +99,14 @@ class Tracker(enum.Enum):
 
 	class THECROSSWORDSOLVER(TrackerBase):
 		method = 'post'
-		@classmethod
-		def url(cls, clue : str) -> str:
+		def url(self) -> str:
 			return 'http://www.the-crossword-solver.com/search'
-		@classmethod
-		def formdata(cls, clue : str) -> aiohttp.FormData:
+		def formdata(self) -> aiohttp.FormData:
 			formdata = aiohttp.FormData()
-			formdata.add_field('q', clue)
+			formdata.add_field('q', self.clue)
 			return formdata
-		@classmethod
-		async def get_scores(cls, clue : str, session : aiohttp.ClientSession) -> Dict[str, float]:
-			html = await cls.fetch(clue, session)
+		async def get_scores(self) -> Dict[str, float]:
+			html = await self.fetch()
 			answer_scores = {} # type: Dict[str, float]
 			regex = r'<p class="searchresult"><a[^>]*>([\w\s]+)</a><span class="matchtypes">(?:<span[^>]*>[^<]*</span>\s*)*</span>'
 			matches = re.findall(regex, html)
@@ -117,18 +115,35 @@ class Tracker(enum.Enum):
 			return answer_scores
 
 	class WORDPLAYS(TrackerBase):
-		# requires user agent to be set to non-default
+		'''
+		WORDPLAYS and ACROSSNDOWN use the same server. Both sites limit the number of actie connections.
+		Requires user agent to be set to non-default.
+
+		NB: This is nondeterministic because ACROSSNDOWN removes spaces.
+		'''
 		method = 'get'
-		semaphore = asyncio.Semaphore(12)
-		@classmethod
-		def url(cls, clue : str) -> str:
-			return 'https://www.wordplays.com/crossword-solver/{}'.format(urlformat(clue, dash=True))
-		@classmethod
-		async def get_scores(cls, clue : str, session : aiohttp.ClientSession) -> Dict[str, float]:
-			html = await cls.fetch(clue, session)
+		num_trials = 4 # try each site twice
+		sites = [
+			('https://www.wordplays.com/crossword-solver/{}', asyncio.Semaphore(12)),
+			('http://www.acrossndown.com/crosswords/clues/{}', asyncio.Semaphore(12)),
+		]
+		@property
+		def semaphore(self):
+			return self.sites[self.index][1]
+		@property
+		def index(self):
+			return (self.trial + self.trial_offset) % len(self.sites)
+		def __init__(self, clue : str, session : aiohttp.ClientSession):
+			super().__init__(clue, session)
+			self.trial_offset = random.randrange(len(self.sites))
+		def url(self) -> str:
+			site = self.sites[self.index][0]
+			return site.format(self.url_clue(dash=True))
+		async def get_scores(self) -> Dict[str, float]:
+			html = await self.fetch()
 			answer_scores = {} # type: Dict[str, float]
 			star_tag = r'<div></div>'
-			regex = r'<tr[^>]*><td><div class=stars>((?:{})*)</div><div class=clear></div></td><td><a[^>]*>([\w\s]+)</a>'.format(star_tag)
+			regex = r'<tr[^>]*>\s*<td><div class=stars>((?:{})*)</div><div class=clear></div></td><td><a[^>]*>([\w\s]+)</a>'.format(star_tag)
 			matches = re.findall(regex, html)
 			for match in matches:
 				star_tags, answer = match
@@ -139,12 +154,10 @@ class Tracker(enum.Enum):
 
 	class CROSSWORDNEXUS(TrackerBase):
 		method = 'get'
-		@classmethod
-		def url(cls, clue : str) -> str:
-			return 'https://crosswordnexus.com/finder.php?clue={}'.format(urlformat(clue))
-		@classmethod
-		async def get_scores(cls, clue : str, session : aiohttp.ClientSession) -> Dict[str, float]:
-			html = await cls.fetch(clue, session)
+		def url(self) -> str:
+			return 'https://crosswordnexus.com/finder.php?clue={}'.format(self.url_clue())
+		async def get_scores(self) -> Dict[str, float]:
+			html = await self.fetch()
 			answer_scores = {} # type: Dict[str, float]
 			star_tag = r'<img src="/images/star.png" />'
 			regex = r'<tr[^>]*>\s*<td[^>]*>\s*((?:{})*)\s*</td>\s*<td[^>]*>\s*<big>\s*<a[^>]*>([\w\s]+)</a>'.format(star_tag)
@@ -158,12 +171,10 @@ class Tracker(enum.Enum):
 
 	class CROSSWORDTRACKER(TrackerBase):
 		method = 'get'
-		@classmethod
-		def url(cls, clue : str) -> str:
-			return 'http://crosswordtracker.com/search?clue={}'.format(urlformat(clue, dash=True))
-		@classmethod
-		async def get_scores(cls, clue : str, session : aiohttp.ClientSession) -> Dict[str, float]:
-			html = await cls.fetch(clue, session)
+		def url(self) -> str:
+			return 'http://crosswordtracker.com/search?clue={}'.format(self.url_clue(dash=True))
+		async def get_scores(self) -> Dict[str, float]:
+			html = await self.fetch()
 			answer_scores = {} # type: Dict[str, float]
 			regex = r'<li class="answer" data-count="(\d+)" data-text="([\w\s]+)">'
 			matches = re.findall(regex, html)
@@ -174,12 +185,10 @@ class Tracker(enum.Enum):
 
 	class GOOGLE(TrackerBase):
 		method = 'get'
-		@classmethod
-		def url(cls, clue : str) -> str:
-			return 'https://www.google.com/search?q={}'.format(urlformat(clue))
-		@classmethod
-		async def get_scores(cls, clue : str, session : aiohttp.ClientSession) -> Dict[str, float]:
-			html = await cls.fetch(clue, session)
+		def url(self) -> str:
+			return 'https://www.google.com/search?q={}'.format(self.url_clue())
+		async def get_scores(self) -> Dict[str, float]:
+			html = await self.fetch()
 			answer_scores = {} # type: Dict[str, float]
 			wikipeidia_regex = r'>([^<]*) - Wikipedia<'
 			matches = re.findall(wikipeidia_regex, html)
